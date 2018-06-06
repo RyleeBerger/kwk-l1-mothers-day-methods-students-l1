7d75cee597713b27c446a0f52cc1c2aa4da35825
RyleeBerger <ryleehaddon24@outlook.com>
2018-06-06 20:26:17 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,8 +1,8 @@
-def holiday_greeting("Beyonce", "Jay-z", "Fourth of July")
-  to("Beyonce")
-  from("Jay-z")
-  holiday("Fourth of July")
+def holiday_greeting(to, from, holiday)
+  # to("Beyonce")
+  # from("Jay-z")
+  # holiday("Fourth of July")
   puts "Happy #{holiday}, #{to}! - From #{from}"
 end
 
-holiday_greeting
+holiday_greeting("Beyonce", "Jay-z", "Fourth of July")
